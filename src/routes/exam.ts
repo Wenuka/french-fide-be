0dfa2c4fc4146a28e9ae96b9566f0ae3236b75b6
Wenuka <wenuka@gmail.com>
2026-02-28 03:41:29 +0100

    remove older function

--- src/routes/exam.ts
+++ src/routes/exam.ts
@@ -6,23 +6,6 @@ import { extractUidFromRequest } from "./user/helpers";
 
 import fs from "fs";
 import path from "path";
-import multer from "multer";
-
-const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        const uploadPath = path.join(__dirname, "../../../french-fide/public/audio/recordings");
-        if (!fs.existsSync(uploadPath)) {
-            fs.mkdirSync(uploadPath, { recursive: true });
-        }
-        cb(null, uploadPath);
-    },
-    filename: (req, file, cb) => {
-        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
-        cb(null, `${file.fieldname}-${uniqueSuffix}${path.extname(file.originalname) || '.webm'}`);
-    }
-});
-
-const upload = multer({ storage });
 
 const router = Router();
 
